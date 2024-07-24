5d8570ae3eb8c7102c23ad46b9c6aa950d1e279e
mlakshmanraj <muthu.lakshmanraj@eastwestbank.com>
2024-07-24 16:58:38 +0530

    Palywright automation framework

---
+++ tests-e2e/GTSA2.0.spec.ts
@@ -0,0 +1,142 @@
+
+// =====================================================================
+//
+// Name: END To END - Net New Eform Creation
+// Description: Create new Net New Eform and generate PDF
+// 01. Navigate to GTSA 2.0 UAT
+// 02. Search for a Company ID 
+// 03. Create New eForm
+// 04. Update all mandatory details
+// 05. Generate PDF file
+//
+// =====================================================================
+import { test, expect, type Page } from '@playwright/test';
+import dotenv from 'dotenv';
+dotenv.config();
+import { gtsaUSSearchDashboard,gtsaAlertWindow,gtsaCommonComponents,gtsaListSelector,subAreaMenuSelectors, viewSelectors, 
+  gridSelectors, FormControlSelectors} from "../selectors/GTSA2.0Selectors.json";
+import { getCSVData, navigateToApps, stringFormat, waitUntilAppIdle } from '../utils/common';
+import { allure } from 'allure-playwright';
+import { Severity } from "allure-js-commons";
+
+const customerID:any= process.env.CUSTOMERID?.toString();
+const profileID:any= process.env.PROFILEID?.toString();
+  test.describe('E2E GTSA 2.0 eForm Creation', () => {
+    test.describe.configure({ mode: 'serial' });
+    let appId = process.env.CSH_APPID as string;
+    test.beforeEach(async ({ page }) => {
+      await navigateToApps(page, stringFormat(appId) ,'GTSA 2.0 ');
+    });
+    //const testData =getCSVData('testcase');
+  test('01.Search for ACI migrated Profile', async ({ page },testInfo) => {
+  //Search for profile
+    await waitUntilAppIdle(page);
+    const frame =page.frameLocator(stringFormat(gtsaUSSearchDashboard.frmDashboard,'WebResource_WebResource_bobauscustomersearch-'));
+    await frame.locator(stringFormat(gtsaUSSearchDashboard.btnCompanyID)).click();
+    await frame.locator(stringFormat(gtsaUSSearchDashboard.ddCompanyId)).click();
+    await frame.locator(stringFormat(gtsaUSSearchDashboard.txtSearchCustomerInput)).fill(profileID);
+    await frame.locator(stringFormat(gtsaUSSearchDashboard.btnSearch)).click();
+    await expect(frame.locator(stringFormat(gtsaUSSearchDashboard.tblCompanyId))).toHaveText(profileID, { timeout: 10000 });
+    //const screenshot =await this.page.screenshot({ path: 'screenshot.png', fullPage: true });
+  //  testinfo.attach(testinfo.title,{ body: screenshot, contentType: 'image/png'} );
+    await frame.locator(stringFormat(gtsaUSSearchDashboard.tblCompanyId)).waitFor({state:'visible'});
+    let profileDetails = frame.locator(stringFormat(gtsaUSSearchDashboard.tblCompanyId));
+    await profileDetails.waitFor({state:'attached'});
+    await allure.description(
+      "GTSA 2.0 maintenance company id search.",
+    );
+    await allure.owner("Muthu");
+    await allure.tags("Maintenacen", "BBP");
+    await allure.severity(Severity.CRITICAL);
+    await allure.link("https://example.com/docs", "Related Documentation");
+    await allure.issue("AUTH-123", "https://example.com/issues/AUTH-123");
+    await allure.tms("TMS-456", "https://example.com/tms/TMS-456");
+    
+  });
+  
+    test('02.Create GTSA 2.0 Net New eForm', async ({ page },testInfo) => {
+    let testData: any;
+    await getCSVData(testInfo.title,'testcase').then((value)=>{testData =JSON.parse(value)});
+    //Create profile
+    await waitUntilAppIdle(page);
+    const frame =page.frameLocator(stringFormat(gtsaUSSearchDashboard.frmDashboard,'WebResource_WebResource_bobauscustomersearch-'));
+    await frame.locator(stringFormat(gtsaUSSearchDashboard.txtSearchCustomerInput)).fill(customerID);
+    await frame.locator(stringFormat(gtsaUSSearchDashboard.btnSearch)).click();
+    await page.locator(stringFormat(gtsaAlertWindow.AlertButton,'alertdialog','okButton')).waitFor({state:'attached'});
+    await page.locator(stringFormat(gtsaAlertWindow.AlertButton,'alertdialog','okButton')).waitFor({state:'visible'});
+    page.on('dialog', dialog => console.log('---- dialig '+dialog.message()));
+    await expect(page.locator(stringFormat(gtsaAlertWindow.AlertButton,'alertdialog','okButton'))).toHaveText("Ok", { timeout: 10000 });
+    await page.locator(stringFormat(gtsaAlertWindow.AlertButton,'alertdialog','okButton')).click();
+    await frame.locator(stringFormat(gtsaCommonComponents.StaticButton,'Create New Profile')).scrollIntoViewIfNeeded();
+    await frame.locator(stringFormat(gtsaCommonComponents.StaticButton,'Create New Profile')).waitFor({state:'visible'});
+    await frame.locator(stringFormat(gtsaCommonComponents.StaticButton,'Create New Profile')).click();
+    await page.locator(stringFormat(gtsaListSelector.TabList,'eForm Form','Company Entitlements')).waitFor({state:'visible'}); 
+    await page.locator(stringFormat(FormControlSelectors.OptionSet,'ewb_businesspackages')).click();
+    await page.locator(stringFormat(gtsaListSelector.DivSelectBox,'listbox','option','Standard')).click();
+    //Account Entitlement
+    await page.locator(stringFormat(gtsaListSelector.TabList,'eForm Form','Account Entitlements')).click();
+    await page.locator(stringFormat(gtsaCommonComponents.DynamicButton,'menuitem','Add Accounts')).click();
+    await page.locator(stringFormat(subAreaMenuSelectors.DynamicSubMenuSelector,'menu','Add Accounts')).click();
+    await page.waitForSelector(stringFormat(gtsaCommonComponents.SpinnerSelector),{state:'hidden'});
+    console.log('Row billing account  element'+stringFormat(gridSelectors.GridRowsColumnInputSector,'1','10'));
+    console.log('Row billing account  element'+stringFormat(gridSelectors.GridRowsColumnInputSector,'1','1'));
+    console.log('Row billing account  element'+stringFormat(gridSelectors.GridRowsColumnInputSector,'3','1'));
+    const accountFrame=page.frameLocator(stringFormat(gtsaUSSearchDashboard.frmDashboard,'MscrmControls.WebResource.WebResourceHtmlControl-FullPageWebResource'));
+    await accountFrame.locator(stringFormat(gridSelectors.GridRowsColumnInputSector,'1','10')).waitFor({state:'visible'});
+    await accountFrame.locator(stringFormat(gridSelectors.GridRowsColumnInputSector,'4','10')).click(); 
+    await page.locator(stringFormat(gtsaAlertWindow.AlertButton,'confirmdialog','confirmButton')).click();
+    await page.locator(stringFormat(gtsaAlertWindow.AlertButton,'alertdialog','okButton')).click();
+    await accountFrame.locator(stringFormat(gridSelectors.GridRowsColumnInputSector,'1','2')).click();
+    await accountFrame.locator(stringFormat(gridSelectors.GridRowsColumnInputSector,'3','2')).click(); 
+    await accountFrame.locator(stringFormat(gtsaCommonComponents.StaticButton,'Add Account Entitlements')).scrollIntoViewIfNeeded();
+    await accountFrame.locator(stringFormat(gtsaCommonComponents.StaticButton,'Add Account Entitlements')).waitFor({state:'attached'});
+    await accountFrame.locator(stringFormat(gtsaCommonComponents.StaticButton,'Add Account Entitlements')).waitFor({state:'visible'});
+    await accountFrame.locator(stringFormat(gtsaCommonComponents.StaticButton,'Add Account Entitlements')).click();
+    await page.waitForSelector(stringFormat(gtsaCommonComponents.SpinnerSelector),{state:'hidden'});
+    await page.locator(stringFormat(gtsaAlertWindow.AlertButton,'alertdialog','okButton')).click();
+    await accountFrame.locator(stringFormat(gtsaCommonComponents.StaticButton,'Close')).click();
+    await page.waitForSelector(stringFormat(gtsaCommonComponents.SpinnerSelector),{state:'hidden'});
+    if(await page.locator(stringFormat(gtsaListSelector.TabList,'eForm Form','Online Administrator')).isHidden()){
+      console.log("Online admin is Hidded")
+      await page.locator(stringFormat(gtsaListSelector.TabList,'eForm Form','More Tabs')).click();
+      await page.locator(stringFormat(gtsaListSelector.DivMenuBox,'menu','menuitem','Online Administrator')).click();
+    }
+    else{
+      await page.locator(stringFormat(gtsaListSelector.TabList,'eForm Form','Online Administrator')).click();
+    }
+      //Adding user details
+    await page.locator(stringFormat(gtsaCommonComponents.DynamicButton,'menuitem','New User. Add New User')).click();
+    await page.waitForSelector(stringFormat(gtsaCommonComponents.SpinnerSelector),{state:'hidden'});
+    await page.locator(stringFormat(FormControlSelectors.TextBox,'ewb_firstname')).fill(testData.User1_FirstName);
+    await page.locator(stringFormat(FormControlSelectors.TextBox,'ewb_lastname')).fill(testData.User1_LastName);
+    await page.locator(stringFormat(FormControlSelectors.MailTextBox,'ewb_administratoremailaddress')).fill(testData.User1_Email);
+    await page.locator(stringFormat(FormControlSelectors.ToggleContainer,'ewb_signer')).click();
+    await page.locator(stringFormat(FormControlSelectors.PhoneTextBox,'ewb_administratorphone')).fill(testData.User1_PhoneNumer_1);
+    await page.locator(stringFormat(subAreaMenuSelectors.DynamicSubAreaMenuSelector,'Commands','Save & Close')).click();
+    await page.waitForSelector(stringFormat(gtsaCommonComponents.SpinnerSelector),{state:'hidden'});
+    await page.locator(stringFormat(gtsaCommonComponents.DynamicButton,'menuitem','New User. Add New User')).click();
+    await page.locator(stringFormat(FormControlSelectors.TextBox,'ewb_firstname')).fill('Second User');
+    await page.locator(stringFormat(FormControlSelectors.TextBox,'ewb_lastname')).fill('4Last Name');
+    await page.locator(stringFormat(FormControlSelectors.MailTextBox,'ewb_administratoremailaddress')).fill('email2@gmail.com');
+    await page.locator(stringFormat(FormControlSelectors.ToggleContainer,'ewb_signer')).click();
+    await page.locator(stringFormat(FormControlSelectors.PhoneTextBox,'ewb_administratorphone')).fill('123456');
+    await page.locator(stringFormat(subAreaMenuSelectors.DynamicSubAreaMenuSelector,'Commands','Save & Close')).click();
+    //Generate PDF
+
+    
+    //const screenshot =await this.page.screenshot({ path: 'screenshot.png', fullPage: true });
+    //testinfo.attach(testinfo.title,{ body: screenshot, contentType: 'image/png'} );
+    
+    await allure.description(
+      "GTSA 2.0 New New eForm creation \n\n Note that maker creates the new eForm for the customer",
+    );
+    
+    await allure.owner("Muthu");
+    await allure.tags("NeweForm", "BBP");
+    await allure.severity(Severity.CRITICAL);
+    await allure.link("https://example.com/docs", "Related Documentation");
+    await allure.issue("AUTH-123", "https://example.com/issues/AUTH-123");
+    await allure.tms("TMS-456", "https://example.com/tms/TMS-456");
+  });
+
+});
---
+++ utils/login.ts
@@ -0,0 +1,27 @@
+import { Page } from '@playwright/test';
+import { isElementVisible} from './common';
+async function login(
+  page: Page,
+  orgurl: string,
+  username: string,
+  password: string,
+
+): Promise<void> {
+  console.log("-----"+orgurl)
+  await page.goto(orgurl);
+  if(!isElementVisible(page, "GTSA - US - Search Dashboard")){
+  await page.getByPlaceholder('Email, phone, or Skype').click();
+  await page.getByPlaceholder('Email, phone, or Skype').fill(username);
+  await page.getByRole('button', { name: 'Next' }).click();
+  await page.getByPlaceholder('Password').click();
+  await page.getByPlaceholder('Password').fill(password);
+  await Promise.all([
+    page.waitForNavigation(),
+    await page.getByRole('button', { name: 'Sign in' }).click(),
+    await page.getByRole('button', { name: 'Yes' }).click(),
+  ]);
+  }else{
+  console.log('logged in into GTSA ')
+  }
+}
+export default login;
